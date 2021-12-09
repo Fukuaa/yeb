982deee353fdea0b13aeca5753ccf7552424146a
zqf <1936534366@qq.com>
2021-12-10 00:25:53 +0800

    2021年12月10日00点24分

---
+++ yeb-generator/src/main/java/com/xxxx/mapper/EmployeeMapper.java
@@ -0,0 +1,16 @@
+package com.xxxx.mapper;
+
+import com.xxxx.pojo.Employee;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author zouqingfeng
+ * @since 2021-12-10
+ */
+public interface EmployeeMapper extends BaseMapper<Employee> {
+
+}
---
+++ yeb-server/src/main/java/com/xxxx/server/controller/EmployeeController.java
@@ -0,0 +1,37 @@
+package com.xxxx.server.controller;
+
+
+import com.xxxx.server.pojo.Employee;
+import com.xxxx.server.pojo.RespPageBean;
+import com.xxxx.server.service.IEmployeeService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author zouqingfeng
+ * @since 2021-12-10
+ */
+@RestController
+@RequestMapping("/employee/basic")
+public class EmployeeController {
+    @Autowired
+    private IEmployeeService employeeService;
+    @ApiOperation("获取所有员工(分页)")
+    @GetMapping("/")
+    public RespPageBean getEmployee(@RequestParam(defaultValue = "1") Integer currentPage,
+                                    @RequestParam(defaultValue = "10") Integer size,
+                                    Employee employee,
+                                    LocalDate[] beginDateScope){
+        return employeeService.getEmployeeByPage(currentPage,size,employee,beginDateScope);
+    }
+}
