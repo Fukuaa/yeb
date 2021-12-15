1f3159e561bca80fe6635a890a387784c39134e7
zqf <1936534366@qq.com>
2021-12-16 00:27:14 +0800

    未完成fastdfs

--- yeb-server/src/main/java/com/xxxx/server/pojo/RespBean.java
+++ yeb-server/src/main/java/com/xxxx/server/pojo/RespBean.java
@@ -18,9 +18,9 @@ public class RespBean {
         return new RespBean(200,message,obj);
     }
     public static RespBean error(String message){
-        return new RespBean(200,message,null);
+        return new RespBean(500,message,null);
     }
     public static RespBean error(String message,Object obj){
-        return new RespBean(200,message,obj);
+        return new RespBean(500,message,obj);
     }
 }
